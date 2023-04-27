27cf4b89ba9bebbe4ba8646cbdc4fee1048ba8a3
pashunt13 <pavelkozencev2002@gmail.com>
2023-04-27 12:25:08 +0300

    Rewrite Board to use two loops

--- src/App.js
+++ src/App.js
@@ -31,24 +31,30 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {rows.map((row) => {
+        return (
+          <div className="board-row" key={row}>
+            {row.map((square) => {
+              return (
+                <Square
+                  key={square}
+                  value={squares[square]}
+                  onSquareClick={() => handleClick(square)}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </>
   );
 }
